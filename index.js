e6484320aeac2323955519beace48b871f4c18d8
Justin Mao <justin.mao@mail.utoronto.ca>
2018-08-10 15:24:21 -0400

    Add language support

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 (function () {
+
+  function checkWidth() {
+    var enop = document.getElementById('enop');
+    var frop = document.getElementById('frop');
+    var cnop = document.getElementById('cnop');
+    if (window.innerWidth < 600) {
+      enop.innerHTML = 'en';
+      frop.innerHTML = 'fr';
+      cnop.innerHTML = 'cn';
+      cnop.classList.remove('cn');
+    } else {
+      enop.innerHTML = 'english';
+      frop.innerHTML = 'français';
+      cnop.innerHTML = '中文';
+      cnop.classList.add('cn');
+    }
+  }
+
+  function setLang(lang) {
+    var en = document.getElementById('en');
+    var fr = document.getElementById('fr');
+    var cn = document.getElementById('cn');
+    en.style.display = 'none';
+    fr.style.display = 'none';
+    cn.style.display = 'none';
+    if (lang == 'en') {
+      en.style.display = 'flex';
+    } else if (lang == 'fr') {
+      fr.style.display = 'flex';
+    } else if (lang == 'cn') {
+      cn.style.display = 'flex';
+    }
+  }
+
+  window.onresize = checkWidth;
   window.onload = () => {
+    // Set header
+    checkWidth();
+    // Set click listeners
+    document.getElementById('enop').addEventListener('click', () => {
+      setLang('en')
+    });
+    document.getElementById('frop').addEventListener('click', () => {
+      setLang('fr')
+    });
+    document.getElementById('cnop').addEventListener('click', () => {
+      setLang('cn')
+    });
+    // Fade in
     setTimeout(() => {
-      document.getElementById("save-the-date").style.opacity = 1;
+      document.getElementById("fade-in").style.opacity = 1;
     }, 500);
   }
 })();
